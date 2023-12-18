3064b937e761e84913de546e743c2495b2a2075a
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2023-12-18 21:49:16 +0700

    Solve the challenge

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -18,13 +19,38 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// The Challenge
+app.get("/api/:date?", function (req, res) {
+  const date = req.params.date;
 
+  if (date === undefined || date === null) {
+    return res.json({
+      unix: new Date().valueOf(),
+      utc: new Date().toUTCString()
+    });
+  }
+
+  let timestamp
+  if (/^\d+$/.test(date)) {
+    timestamp = parseInt(date)
+  } else {
+    const parsedDate = new Date(date)
+
+    if (isNaN(parsedDate)) {
+      return res.json({ error: "Invalid Date" })
+    }
+
+    timestamp = parsedDate.getTime()
+  }
+
+  res.json({ unix: timestamp, utc: new Date(timestamp).toUTCString() })
+  
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
